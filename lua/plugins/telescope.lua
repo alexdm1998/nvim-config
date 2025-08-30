1d9ce8e77fc6662453195127d4f6ee4979754fba
Alex <alexdm1998@gmail.com>
2025-08-30 20:09:58 +0100

    Config repo creation

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	keys = {
+		{ "<Leader>ff", "<Cmd>Telescope find_files<CR>", desc = "Find Files"},
+		{ "<Leader>fg", "<Cmd>Telescope live_grep<CR>",  desc = "Live Grep"},
+		{ "<Leader>fb", "<Cmd>Telescope buffers<CR>",    desc = "Telescope buffers"},
+		{ "<Leader>fh", "<Cmd>Telescope help_tags<CR>",  desc = "Help Tags"}
+	}
+
+    }
